b318d7887066298e8b938bdc7495b7071c307e96
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-02 19:53:44 +0300

    add backend project

---
+++ backend/src/main/java/com/roko/hotelpricefollower/HotelPriceFollowerApplication.java
@@ -0,0 +1,13 @@
+package com.roko.hotelpricefollower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelPriceFollowerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelPriceFollowerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/roko/hotelpricefollower/HotelPriceFollowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roko.hotelpricefollower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelPriceFollowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
