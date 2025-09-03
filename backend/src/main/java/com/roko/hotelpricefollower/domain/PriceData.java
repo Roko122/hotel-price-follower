49c46d31a6df39ee4841f3d25de4431194ed5fe9
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-03 21:43:19 +0300

    add necessary fields and annotations to PriceData.java

--- backend/src/main/java/com/roko/hotelpricefollower/domain/PriceData.java
+++ backend/src/main/java/com/roko/hotelpricefollower/domain/PriceData.java
@@ -1,4 +1,36 @@
 package com.roko.hotelpricefollower.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Table(name = "price_data")
 public class PriceData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "price_in_cents", precision = 6, scale = 0)
+    private Long priceInCents;
+
+    @Column(name = "departure_date")
+    private LocalDate departureDate;
+
+    @Column(name = "sold_out")
+    private boolean soldOut;
+
+    @Column(name = "additional_info")
+    private String additionalInfo;
+
+    @JoinColumn(name = "room_id", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Room room;
+
 }
