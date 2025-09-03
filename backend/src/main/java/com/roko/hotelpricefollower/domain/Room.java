99ec807263825f575d3b7a5fe0e3f6d6ba210d42
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-03 21:00:28 +0300

    add necessary fields and annotations to Room.java

--- backend/src/main/java/com/roko/hotelpricefollower/domain/Room.java
+++ backend/src/main/java/com/roko/hotelpricefollower/domain/Room.java
@@ -1,4 +1,24 @@
 package com.roko.hotelpricefollower.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Table(name = "Rooms")
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "type")
+    private String type;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hotel_id", nullable = false)
+    private Hotel hotel;
 }
