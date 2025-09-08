b01ec50f3e8864b6f866eda9d727a6632931c027
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-08 13:49:17 +0300

    add helper functions to poll for price data and a public method to fetch the data

--- backend/src/main/java/com/roko/hotelpricefollower/scraper/HotelScraper.java
+++ backend/src/main/java/com/roko/hotelpricefollower/scraper/HotelScraper.java
@@ -1,7 +1,12 @@
 package com.roko.hotelpricefollower.scraper;
 
-import org.openqa.selenium.WebDriver;
+import com.roko.hotelpricefollower.exception.PriceMatrixNotFoundException;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 public class HotelScraper {
@@ -12,6 +17,30 @@ public class HotelScraper {
     public HotelScraper(WebDriver driver, HotelParser hotelParser) {
         this.driver = driver;
         this.hotelParser = hotelParser;
+    }
+
+    public void getHotelPrices(String hotelUrl) {
+        WebElement priceMatrix;
+
+        try {
+            priceMatrix = this.getPriceMatrix(hotelUrl);
+        } catch (TimeoutException e) {
+            throw new PriceMatrixNotFoundException("Could not find price matrix, please try again later.\n" + hotelUrl);
+        }
+    }
+
+    private WebElement getPriceMatrix(String hotelUrl) {
+        driver.get(hotelUrl);
+
+        return getWait().until(driver ->
+                driver.findElement(By.className("tcne-pm-main-matrix__block")));
+    }
+
+    private Wait<WebDriver> getWait() {
+        return new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(30L))
+                .pollingEvery(Duration.ofSeconds(15L))
+                .ignoring(NoSuchElementException.class);
     }
 
 
