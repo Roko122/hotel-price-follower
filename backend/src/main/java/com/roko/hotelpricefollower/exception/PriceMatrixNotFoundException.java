991e931acb189134d8a109fd5708bab4248a5749
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-08 13:48:09 +0300

    add PriceMatrixNotFoundException.java

---
+++ backend/src/main/java/com/roko/hotelpricefollower/exception/PriceMatrixNotFoundException.java
@@ -0,0 +1,7 @@
+package com.roko.hotelpricefollower.exception;
+
+public class PriceMatrixNotFoundException extends RuntimeException {
+    public PriceMatrixNotFoundException(String s) {
+        super(s);
+    }
+}
