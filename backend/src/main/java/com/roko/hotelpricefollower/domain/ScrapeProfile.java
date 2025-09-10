27844834d6371ed13d5ca63d9223f0377cb41fa8
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-10 18:40:16 +0300

    add 2-way connection to PriceFetch for easier db operations

--- backend/src/main/java/com/roko/hotelpricefollower/domain/ScrapeProfile.java
+++ backend/src/main/java/com/roko/hotelpricefollower/domain/ScrapeProfile.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 @Table(name = "scrape_profiles")
@@ -30,4 +32,7 @@ public class ScrapeProfile {
     @JoinColumn(name = "hotel_id", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
     private Hotel hotel;
+
+    @OneToMany(mappedBy = "scrapeProfile", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<PriceFetch> priceFetches;
 }
