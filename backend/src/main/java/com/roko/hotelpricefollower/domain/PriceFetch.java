a9472bad748ab07199b5f402c231ac05fc805e50
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-10 18:41:43 +0300

    add delete on cascade

--- backend/src/main/java/com/roko/hotelpricefollower/domain/PriceFetch.java
+++ backend/src/main/java/com/roko/hotelpricefollower/domain/PriceFetch.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.time.Instant;
 
@@ -28,5 +30,6 @@ public class PriceFetch {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "scrape_profile_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private ScrapeProfile scrapeProfile;
 }
