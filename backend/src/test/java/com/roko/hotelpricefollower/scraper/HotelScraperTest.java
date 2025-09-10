5c7f8ef513daf54e7e19e950d7465ac2d65f202d
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-10 18:06:18 +0300

    fix test

--- backend/src/test/java/com/roko/hotelpricefollower/scraper/HotelScraperTest.java
+++ backend/src/test/java/com/roko/hotelpricefollower/scraper/HotelScraperTest.java
@@ -1,11 +1,11 @@
 package com.roko.hotelpricefollower.scraper;
 
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebElement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class HotelScraperTest {
@@ -16,8 +16,9 @@ public class HotelScraperTest {
     private final String testUrl = "https://www.tjareborg.fi/kanariansaaret/teneriffa/playa-de-las-americas/sunprime-ocean-view?UseBookingFlow=true&QueryDepID=12728&QueryResID=12190&QueryDepDate=20251207&QueryUnits=0&QueryAges=42%2C42&QueryDur=8&ItemId=89669&qf=flowCharter&selectedTransport=flight%7CCgJURhIkMzAyYjUzMDgtOTcxZS00OWE2LWFkMTYtODhiMzBjYjNmY2Ex&SelectedFlightClass=&RoomKey=hotel%7CCgJURhIDVEZTGgRTVU9WIgZBMTJCQUw&SelectedMeals=#hotel-search-app-root";
 
     @Test
-    public void getHotelPriceMatrixReturnsAnElement() {
-        WebElement priceMatrix = hotelScraper.getPriceMatrix(testUrl);
+    public void getHotelPriceMatrixReturnsCorrectHTMLAsAString() {
+        String priceMatrix = hotelScraper.scrapePriceMatrix(testUrl);
         assertNotNull(priceMatrix, "priceMatrix is null");
+        assertTrue(priceMatrix.contains("tcne-pm-matrix"));
     }
 }
