38f1dae86e579f36a114fe51003470da89a67cad
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-11 10:18:21 +0300

    add a test for RoomPriceParser

---
+++ backend/src/test/java/com/roko/hotelpricefollower/scraper/parser/RoomPriceParserTest.java
@@ -0,0 +1,41 @@
+package com.roko.hotelpricefollower.scraper.parser;
+
+import com.roko.hotelpricefollower.scraper.parser.RoomPriceParser.ParsedRoomPrice;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class RoomPriceParserTest {
+    @Autowired
+    private RoomPriceParser roomPriceParser;
+
+    private File exampleHTML;
+    private Document priceMatrixDoc;
+
+    public RoomPriceParserTest() throws IOException {
+        this.exampleHTML = new File("src/test/resources/html/price_matrix.html");
+        this.priceMatrixDoc = Jsoup.parse(exampleHTML, "UTF-8");
+    }
+
+    @Test
+    public void testThatRoomPriceParserWorks() {
+        Map<String, List<ParsedRoomPrice>> result = roomPriceParser.parseRoomsAndPrices(priceMatrixDoc);
+        assertFalse(result.isEmpty(), "Result should not be empty");
+        System.out.println(result);
+
+        assertTrue(result.containsKey("CLASSIC STUDIO - 1 huone, parveke allasalueelle päin"),
+                "Result should contain specific room");
+    }
+
+}
