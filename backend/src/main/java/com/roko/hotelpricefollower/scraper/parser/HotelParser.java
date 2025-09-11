9c1a8621da0b4c1176132dfed501dcc61cadbea8
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-11 18:36:47 +0300

    add working HotelParser
    - combines dates and room prices into a RoomPriceData object

--- backend/src/main/java/com/roko/hotelpricefollower/scraper/parser/HotelParser.java
+++ backend/src/main/java/com/roko/hotelpricefollower/scraper/parser/HotelParser.java
@@ -1,31 +1,51 @@
 package com.roko.hotelpricefollower.scraper.parser;
 
-import com.roko.hotelpricefollower.domain.RoomPrice;
+import com.roko.hotelpricefollower.scraper.parser.dto.ParsedRoomPrice;
+import com.roko.hotelpricefollower.scraper.parser.dto.RoomPriceData;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class HotelParser {
 
-    private DateParser dateParser;
-    private RoomParser roomParser;
-    private PriceParser priceParser;
+    private final DateParser dateParser;
+    private final RoomPriceParser roomPriceParser;
 
-    public HotelParser(DateParser dateParser, RoomParser roomParser, PriceParser priceParser) {
+    public HotelParser(DateParser dateParser, RoomPriceParser roomPriceParser) {
         this.dateParser = dateParser;
-        this.roomParser = roomParser;
-        this.priceParser = priceParser;
+        this.roomPriceParser = roomPriceParser;
     }
 
-    public List<RoomPrice> roomPrices(String priceMatrix) {
+    public List<RoomPriceData> parseRoomPrices(String priceMatrix) {
         Document priceMatrixDoc = Jsoup.parse(priceMatrix);
+
         List<LocalDate> parsedDates = dateParser.parseDates(priceMatrixDoc);
+        Map<String, List<ParsedRoomPrice>> parsedRoomsAndPrices = roomPriceParser.parseRoomsAndPrices(priceMatrixDoc);
 
+        return combineRoomPricesAndDates(parsedDates, parsedRoomsAndPrices);
+    }
 
-        return null;
+    private List<RoomPriceData> combineRoomPricesAndDates(List<LocalDate> parsedDates,
+                                                          Map<String, List<ParsedRoomPrice>> parsedRoomsAndPrices) {
+        List<RoomPriceData> roomPricesData = new ArrayList<>();
+
+        parsedRoomsAndPrices.forEach((roomName, parsedRoomPrices) -> {
+            //parsedRoomPrices and parsedDates are always of the same length
+            for (int i = 0; i < parsedDates.size(); i++) {
+                ParsedRoomPrice parsedRoomPrice = parsedRoomPrices.get(i);
+                LocalDate parsedDate = parsedDates.get(i);
+                RoomPriceData roomPriceData = new RoomPriceData(roomName, parsedDate, parsedRoomPrice);
+
+                roomPricesData.add(roomPriceData);
+            }
+        });
+
+        return roomPricesData;
     }
 }
