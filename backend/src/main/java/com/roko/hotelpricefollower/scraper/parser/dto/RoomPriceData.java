396de344270b61d43e9d140b3f9941277f518e38
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-11 18:50:19 +0300

    add test for HotelParser

--- backend/src/main/java/com/roko/hotelpricefollower/scraper/parser/dto/RoomPriceData.java
+++ backend/src/main/java/com/roko/hotelpricefollower/scraper/parser/dto/RoomPriceData.java
@@ -2,11 +2,13 @@ package com.roko.hotelpricefollower.scraper.parser.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class RoomPriceData {
     private String roomType;
     private LocalDate departureDate;
---
+++ backend/src/test/java/com/roko/hotelpricefollower/scraper/parser/HotelParserTest.java
@@ -0,0 +1,37 @@
+package com.roko.hotelpricefollower.scraper.parser;
+
+import com.roko.hotelpricefollower.scraper.parser.dto.RoomPriceData;
+import org.jsoup.Jsoup;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class HotelParserTest {
+
+    @Autowired
+    private HotelParser hotelParser;
+
+    private File exampleHTML;
+    private String priceMatrixString;
+
+    public HotelParserTest() throws IOException {
+        this.exampleHTML = new File("src/test/resources/html/price_matrix.html");
+        this.priceMatrixString = Jsoup.parse(exampleHTML, "UTF-8").toString();
+    }
+
+    @Test
+    public void testThatHotelParserReturnsAListOfRoomPriceData() {
+        List<RoomPriceData> roomPriceDataList = hotelParser.parseRoomPrices(priceMatrixString);
+        System.out.println(roomPriceDataList.size());
+        assertFalse(roomPriceDataList.isEmpty(), "RoomPriceDataList is empty");
+        assertEquals(76, roomPriceDataList.size(),
+                "RoomPriceDataList doesn't contain all room price data");
+    }
+}
