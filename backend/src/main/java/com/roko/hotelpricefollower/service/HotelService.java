410b26d7bf1748131426e7b2db8834cf6c446fc8
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-11 19:45:38 +0300

    add HotelService

---
+++ backend/src/main/java/com/roko/hotelpricefollower/service/HotelService.java
@@ -0,0 +1,20 @@
+package com.roko.hotelpricefollower.service;
+
+import com.roko.hotelpricefollower.domain.Hotel;
+import com.roko.hotelpricefollower.repository.HotelRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class HotelService {
+    private final HotelRepository hotelRepository;
+
+    public HotelService(HotelRepository hotelRepository) {
+        this.hotelRepository = hotelRepository;
+    }
+
+    public Optional<Hotel> getHotel(Long hotelId) {
+        return hotelRepository.findById(hotelId);
+    }
+}
