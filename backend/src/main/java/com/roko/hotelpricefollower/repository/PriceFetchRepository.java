182e2dca6158baf47cb532152b4743dcb33e78a0
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-12 09:05:03 +0300

    add find for most recent PriceFetch

--- backend/src/main/java/com/roko/hotelpricefollower/repository/PriceFetchRepository.java
+++ backend/src/main/java/com/roko/hotelpricefollower/repository/PriceFetchRepository.java
@@ -1,9 +1,13 @@
 package com.roko.hotelpricefollower.repository;
 
 import com.roko.hotelpricefollower.domain.PriceFetch;
+import com.roko.hotelpricefollower.domain.ScrapeProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PriceFetchRepository extends JpaRepository<PriceFetch, Long> {
+    Optional<PriceFetch> findFirstByScrapeProfileOrderByFetchTimeDesc(ScrapeProfile scrapeProfile);
 }
