c2f3901964a1c4c9baba4e2c57672b1c245bb11a
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-12 09:06:33 +0300

    add PriceFetchService
    - method to create a new PriceFetch
    - method to save a PriceFetch

---
+++ backend/src/main/java/com/roko/hotelpricefollower/service/PriceFetchService.java
@@ -0,0 +1,35 @@
+package com.roko.hotelpricefollower.service;
+
+import com.roko.hotelpricefollower.domain.PriceFetch;
+import com.roko.hotelpricefollower.domain.ScrapeProfile;
+import com.roko.hotelpricefollower.repository.PriceFetchRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Optional;
+
+@Service
+public class PriceFetchService {
+
+    private final PriceFetchRepository priceFetchRepository;
+
+    public PriceFetchService(PriceFetchRepository priceFetchRepository) {
+        this.priceFetchRepository = priceFetchRepository;
+    }
+
+    public Optional<PriceFetch> getMostRecentPriceFetch(ScrapeProfile scrapeProfile) {
+        return priceFetchRepository.findFirstByScrapeProfileOrderByFetchTimeDesc(scrapeProfile);
+    }
+
+    public PriceFetch createPriceFetch(ScrapeProfile scrapeProfile) {
+        PriceFetch priceFetch = new PriceFetch();
+        priceFetch.setScrapeProfile(scrapeProfile);
+        priceFetch.setFetchTime(Instant.now());
+
+        return priceFetchRepository.save(priceFetch);
+    }
+
+    public void savePriceFetch(PriceFetch priceFetch) {
+        priceFetchRepository.save(priceFetch);
+    }
+}
