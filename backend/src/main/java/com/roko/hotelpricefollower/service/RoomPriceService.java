db66f467b5dc9cc8fff8e8ddcd494db0c66c98b0
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-12 09:07:33 +0300

    add RoomPriceService
    - method to save all RoomPrices
    - helper method to create a RoomPrice object

---
+++ backend/src/main/java/com/roko/hotelpricefollower/service/RoomPriceService.java
@@ -0,0 +1,49 @@
+package com.roko.hotelpricefollower.service;
+
+import com.roko.hotelpricefollower.domain.Hotel;
+import com.roko.hotelpricefollower.domain.PriceFetch;
+import com.roko.hotelpricefollower.domain.Room;
+import com.roko.hotelpricefollower.domain.RoomPrice;
+import com.roko.hotelpricefollower.repository.RoomPriceRepository;
+import com.roko.hotelpricefollower.scraper.parser.dto.RoomPriceData;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class RoomPriceService {
+
+    private final RoomPriceRepository roomPriceRepository;
+    private final RoomService roomService;
+
+    public RoomPriceService(RoomPriceRepository roomPriceRepository, RoomService roomService) {
+        this.roomPriceRepository = roomPriceRepository;
+        this.roomService = roomService;
+    }
+
+    @Transactional
+    public void saveRoomPrices(List<RoomPriceData> roomPriceDataList, Hotel hotel, PriceFetch priceFetch) {
+        List<RoomPrice> roomPrices = new ArrayList<>();
+        for (RoomPriceData roomPriceData : roomPriceDataList) {
+            Room room = roomService.getOrCreateRoom(hotel, roomPriceData.getRoomType());
+
+            RoomPrice roomPrice = createRoomPrice(roomPriceData, room, priceFetch);
+            roomPrices.add(roomPrice);
+        }
+        roomPriceRepository.saveAll(roomPrices);
+    }
+
+    private RoomPrice createRoomPrice(RoomPriceData roomPriceData, Room room, PriceFetch priceFetch) {
+
+        return RoomPrice.builder()
+                .priceInCents(roomPriceData.getParsedRoomPrice().getPrice() * 100)
+                .departureDate(roomPriceData.getDepartureDate())
+                .soldOut(roomPriceData.getParsedRoomPrice().getSoldOut())
+                .additionalInfo(roomPriceData.getParsedRoomPrice().getAdditionalInformation())
+                .room(room)
+                .priceFetch(priceFetch)
+                .build();
+    }
+}
