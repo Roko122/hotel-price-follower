add950cb7f1100ff13ce94b500326695d5043a51
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-12 14:36:01 +0300

    add private constructor to disallow object creation

--- backend/src/main/java/com/roko/hotelpricefollower/scraper/ScraperConfig.java
+++ backend/src/main/java/com/roko/hotelpricefollower/scraper/ScraperConfig.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ScraperConfig {
+    private ScraperConfig() {
+    }
 
     public static WebDriver setupChromeDriver() {
         ChromeOptions options = new ChromeOptions();
