8846efc73cbb55207becfbb4d3e64d6ed8fba371
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-13 15:58:08 +0300

    add tests for RoomService

---
+++ backend/src/test/java/com/roko/hotelpricefollower/services/RoomServiceTest.java
@@ -0,0 +1,46 @@
+package com.roko.hotelpricefollower.services;
+
+import com.roko.hotelpricefollower.domain.Hotel;
+import com.roko.hotelpricefollower.domain.Room;
+import com.roko.hotelpricefollower.repository.HotelRepository;
+import com.roko.hotelpricefollower.service.RoomService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class RoomServiceTest {
+
+    @Autowired
+    private RoomService roomService;
+
+    @Autowired
+    private HotelRepository hotelRepository;
+
+    private Hotel getHotel() {
+        //Initially, database contains one hotel with id 1
+        return hotelRepository.findById(1L).get();
+    }
+
+    @Test
+    public void testThatGetOrCreateRoomCreatesARoomWhenItDoesNotExist() {
+        String roomType = "Test Deluxe Room";
+        Room room = roomService.getOrCreateRoom(getHotel(), roomType);
+
+        //First room should get the id 1
+        assertEquals(1L, room.getId(), "Room should have id of 1");
+        assertEquals(roomType, room.getType(), "Room type should be 'Test Deluxe Room'");
+    }
+
+    @Test
+    public void testThatGetOrCreateRoomCreatesARoomWhenItExists() {
+        String roomType = "Test Deluxe Room";
+        Room room = roomService.getOrCreateRoom(getHotel(), roomType);
+        Room result = roomService.getOrCreateRoom(getHotel(), roomType);
+
+        //Test that the same room does not get created twice
+        assertEquals(room.getId(), result.getId(), "Room should have id of 1");
+    }
+}
