b05c4da6b06f7599652ca1763c2c91e29726d034
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-13 17:03:14 +0300

    add a test for ScrapeProfileService

---
+++ backend/src/test/java/com/roko/hotelpricefollower/services/ScrapeProfileServiceTest.java
@@ -0,0 +1,32 @@
+package com.roko.hotelpricefollower.services;
+
+import com.roko.hotelpricefollower.domain.RoomPrice;
+import com.roko.hotelpricefollower.repository.RoomPriceRepository;
+import com.roko.hotelpricefollower.service.ScrapeProfileService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@SpringBootTest
+public class ScrapeProfileServiceTest {
+
+    @Autowired
+    private ScrapeProfileService scrapeProfileService;
+
+    @Autowired
+    private RoomPriceRepository roomPriceRepository;
+
+    @Test
+    public void testThatScrapeProfilesScrapesAndSavesAllRoomPrices() {
+        scrapeProfileService.startScrapes();
+        List<RoomPrice> roomPrices = roomPriceRepository.findAll();
+
+        assertFalse(roomPrices.isEmpty(), "RoomPrice list should not be empty");
+        assertEquals(76, roomPrices.size(), "RoomPrice list does not contain all room price data");
+    }
+}
