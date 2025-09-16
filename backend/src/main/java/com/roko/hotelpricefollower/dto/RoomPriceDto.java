5ba983a92ba1fa3002d0bd8b346a9cecf107ffc4
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-16 18:34:18 +0300

    add: constructor to convert Long to BigDecimal and Instant to LocalDate

--- backend/src/main/java/com/roko/hotelpricefollower/dto/RoomPriceDto.java
+++ backend/src/main/java/com/roko/hotelpricefollower/dto/RoomPriceDto.java
@@ -1,10 +1,13 @@
 package com.roko.hotelpricefollower.dto;
 
+import com.roko.hotelpricefollower.util.DateTimeUtil;
+
 import java.math.BigDecimal;
+import java.time.Instant;
 import java.time.LocalDate;
 
-public record RoomPriceDto(
-        BigDecimal price,
-        LocalDate fetchTime
-){
+public record RoomPriceDto(BigDecimal price, LocalDate fetchTime) {
+    public RoomPriceDto(Long priceInCents, Instant fetchTime) {
+        this(new BigDecimal(priceInCents).movePointLeft(2), DateTimeUtil.toLocalDate(fetchTime));
+    }
 }
