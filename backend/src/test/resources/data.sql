2706d57aa47adb0860aa4463b576fb4cdc71e13e
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-17 15:37:56 +0300

    update: scrape_url to search prices for later dates
    - this ensures that tests work for a longer time period

--- backend/src/test/resources/data.sql
+++ backend/src/test/resources/data.sql
@@ -3,7 +3,7 @@ INSERT INTO hotels (id, name)
 
 INSERT INTO scrape_profiles (id, scrape_url, duration_weeks, adults, children, hotel_id)
     VALUES (1,
-            'https://www.tjareborg.fi/kanariansaaret/teneriffa/playa-de-las-americas/sunprime-ocean-view?UseBookingFlow=true&QueryDepID=12728&QueryResID=12190&QueryDepDate=20251221&QueryUnits=0&QueryAges=42%2C42&QueryDur=8&ItemId=89669&qf=flowCharter&selectedTransport=flight%7CCgJURhIkODU4Mjc1YmItNTI1OC00MDE1LWExYjUtYTIwMmI5M2VhODlh&SelectedFlightClass=&RoomKey=hotel%7CCgJURhIDVEZTGgRTVU9WIgZBMTJCQUw&SelectedMeals=#hotel-search-app-root',
+            'https://www.tjareborg.fi/kanariansaaret/teneriffa/playa-de-las-americas/sunprime-ocean-view?UseBookingFlow=true&QueryDepID=12728&QueryResID=12190&QueryDepDate=20260322&QueryUnits=0&QueryAges=42%2C42&QueryDur=8&ItemId=89669&qf=flowCharter&selectedTransport=flight%7CCgJURhIkZGVmYTYyNTMtZGY4NC00MDI2LTk5ODctMjcxMzdkMmRlZjlh&SelectedFlightClass=&RoomKey=hotel%7CCgJURhIDVEZTGgRTVU9WIgZBMTJCQUw&SelectedMeals=#hotel-search-app-root',
             1,
             2,
             0,
