167f1d33aab5b38764b98ec93f1ac773404ccbc7
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-17 12:37:40 +0300

    add: find all rooms by hotel id

--- backend/src/main/java/com/roko/hotelpricefollower/repository/RoomRepository.java
+++ backend/src/main/java/com/roko/hotelpricefollower/repository/RoomRepository.java
@@ -5,9 +5,12 @@ import com.roko.hotelpricefollower.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
     Optional<Room> findByHotelAndType(Hotel hotel, String type);
+
+    List<Room> findAllByHotel(Hotel hotel);
 }
