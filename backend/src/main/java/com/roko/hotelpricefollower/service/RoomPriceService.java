12fa82a74881658ce55bd3be398bd596ce20d2c9
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-17 11:41:18 +0300

    add: ability to fetch relevant price data from database

--- backend/src/main/java/com/roko/hotelpricefollower/service/RoomPriceService.java
+++ backend/src/main/java/com/roko/hotelpricefollower/service/RoomPriceService.java
@@ -4,23 +4,40 @@ import com.roko.hotelpricefollower.domain.Hotel;
 import com.roko.hotelpricefollower.domain.PriceFetch;
 import com.roko.hotelpricefollower.domain.Room;
 import com.roko.hotelpricefollower.domain.RoomPrice;
+import com.roko.hotelpricefollower.dto.PriceDetailsDto;
+import com.roko.hotelpricefollower.dto.RoomPriceDto;
+import com.roko.hotelpricefollower.dto.RoomPriceSummaryDto;
+import com.roko.hotelpricefollower.mapper.RoomPriceMapper;
 import com.roko.hotelpricefollower.repository.RoomPriceRepository;
 import com.roko.hotelpricefollower.scraper.parser.dto.RoomPriceData;
+import com.roko.hotelpricefollower.service.validation.RoomPriceValidator;
+import com.roko.hotelpricefollower.util.DateTimeUtil;
+import org.springframework.data.domain.Limit;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RoomPriceService {
 
     private final RoomPriceRepository roomPriceRepository;
     private final RoomService roomService;
+    private final RoomPriceValidator roomPriceValidator;
+    private final RoomPriceMapper roomPriceMapper;
 
-    public RoomPriceService(RoomPriceRepository roomPriceRepository, RoomService roomService) {
+    public RoomPriceService(RoomPriceRepository roomPriceRepository,
+                            RoomService roomService,
+                            RoomPriceValidator roomPriceValidator,
+                            RoomPriceMapper roomPriceMapper) {
+
         this.roomPriceRepository = roomPriceRepository;
         this.roomService = roomService;
+        this.roomPriceValidator = roomPriceValidator;
+        this.roomPriceMapper = roomPriceMapper;
     }
 
     @Transactional
@@ -33,6 +50,36 @@ public class RoomPriceService {
             roomPrices.add(roomPrice);
         }
         roomPriceRepository.saveAll(roomPrices);
+    }
+
+    @Transactional(readOnly = true)
+    public List<RoomPriceSummaryDto> getRoomPriceSummary(Long hotelId,
+                                                         Long profileId,
+                                                         Long roomId,
+                                                         List<LocalDate> departureDateList) {
+        //Throws an exception if request is not valid
+        roomPriceValidator.validateRequest(hotelId, roomId, profileId);
+
+        ArrayList<RoomPriceSummaryDto> roomPriceSummaries = new ArrayList<>();
+        for (LocalDate departureDate : departureDateList) {
+            PriceDetailsDto mostRecentPrice = createMostRecentPrice(departureDate, roomId, profileId);
+            PriceDetailsDto min30DaysPrice = createMin30DaysPrice(departureDate, roomId, profileId);
+            PriceDetailsDto allTimeMinPrice = createAllTimeMin(departureDate, roomId, profileId);
+
+            RoomPriceSummaryDto priceSummary = roomPriceMapper.toRoomPriceSummaryDto(
+                    departureDate, mostRecentPrice, min30DaysPrice, allTimeMinPrice);
+
+            roomPriceSummaries.add(priceSummary);
+        }
+
+        return roomPriceSummaries;
+    }
+
+    public List<RoomPriceDto> getAllRoomPrices(Long hotelId, Long profileId, Long roomId, LocalDate departureDate) {
+        //Throws an exception if request is not valid
+        roomPriceValidator.validateRequest(hotelId, roomId, profileId);
+
+        return roomPriceRepository.findAllPricesByDepartureDate(departureDate, roomId, profileId);
     }
 
     private RoomPrice createRoomPrice(RoomPriceData roomPriceData, Room room, PriceFetch priceFetch) {
@@ -50,5 +97,43 @@ public class RoomPriceService {
                 .room(room)
                 .priceFetch(priceFetch)
                 .build();
+    }
+
+    private PriceDetailsDto createAllTimeMin(LocalDate departureDate, Long roomId, Long profileId) {
+        Optional<RoomPrice> allTimeMinPrice = roomPriceRepository.findAllTimeMin(
+                departureDate,
+                roomId,
+                profileId,
+                Limit.of(1));
+
+        return allTimeMinPrice
+                .map(roomPriceMapper::toPriceDetailsDto)
+                .orElse(null);
+    }
+
+    private PriceDetailsDto createMin30DaysPrice(LocalDate departureDate, Long roomId, Long profileId) {
+        Optional<RoomPrice> min30DaysPrice = roomPriceRepository.findMin30DaysPrice(
+                departureDate,
+                roomId,
+                profileId,
+                DateTimeUtil.instant30DaysAgo(),
+                DateTimeUtil.instantNow(),
+                Limit.of(1));
+
+        return min30DaysPrice
+                .map(roomPriceMapper::toPriceDetailsDto)
+                .orElse(null);
+    }
+
+    private PriceDetailsDto createMostRecentPrice(LocalDate departureDate, Long roomId, Long profileId) {
+        Optional<RoomPrice> latestPrice = roomPriceRepository.findMostRecentPrice(
+                departureDate,
+                roomId,
+                profileId,
+                Limit.of(1));
+
+        return latestPrice
+                .map(roomPriceMapper::toPriceDetailsDto)
+                .orElse(null);
     }
 }
