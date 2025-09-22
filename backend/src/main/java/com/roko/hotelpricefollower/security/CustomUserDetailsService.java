681e6dc96dd483782dfab3f682fbc6e7b256e983
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-22 18:34:04 +0300

    add: CustomUserDetailsService

---
+++ backend/src/main/java/com/roko/hotelpricefollower/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.roko.hotelpricefollower.security;
+
+import com.roko.hotelpricefollower.domain.User;
+import com.roko.hotelpricefollower.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username" + username));
+
+        return new CustomUserDetails(user);
+    }
+}
