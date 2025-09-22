7a3b7dc5b5d58dbe7a51f06365de95f733a2cb4c
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-22 18:20:31 +0300

    add: method to get GrantedAuthority from a role

--- backend/src/main/java/com/roko/hotelpricefollower/domain/Role.java
+++ backend/src/main/java/com/roko/hotelpricefollower/domain/Role.java
@@ -1,5 +1,12 @@
 package com.roko.hotelpricefollower.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 public enum Role {
-    USER, ADMIN
+    USER, ADMIN;
+
+    public GrantedAuthority getGrantedAuthority() {
+        return new SimpleGrantedAuthority("ROLE_" + this.name());
+    }
 }
