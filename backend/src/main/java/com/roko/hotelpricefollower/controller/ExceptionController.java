148d9996410f3c98670a71c947903f42bcc411be
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-22 18:36:19 +0300

    add: exception handling for BadCredentialsException

--- backend/src/main/java/com/roko/hotelpricefollower/controller/ExceptionController.java
+++ backend/src/main/java/com/roko/hotelpricefollower/controller/ExceptionController.java
@@ -5,6 +5,7 @@ import com.roko.hotelpricefollower.exception.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -31,5 +32,15 @@ public class ExceptionController {
                 .build();
 
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorResponse> handleBadCredentialExceptionException(NotFoundException ex) {
+        ErrorResponse error = ErrorResponse.builder()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .message("Incorrect username or password")
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
 }
