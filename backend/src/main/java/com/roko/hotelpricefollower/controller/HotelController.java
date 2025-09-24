55bd89610d4da6f69bf92032c6fce6f5b830de74
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-24 21:00:00 +0300

    Add an endpoint to create a new hotel

--- backend/src/main/java/com/roko/hotelpricefollower/controller/HotelController.java
+++ backend/src/main/java/com/roko/hotelpricefollower/controller/HotelController.java
@@ -1,10 +1,12 @@
 package com.roko.hotelpricefollower.controller;
 
+import com.roko.hotelpricefollower.domain.Hotel;
+import com.roko.hotelpricefollower.dto.CreateHotelRequest;
 import com.roko.hotelpricefollower.dto.HotelDto;
+import com.roko.hotelpricefollower.mapper.HotelMapper;
 import com.roko.hotelpricefollower.service.HotelService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,13 +15,23 @@ import java.util.List;
 public class HotelController {
 
     private final HotelService hotelService;
+    private final HotelMapper hotelMapper;
 
-    public HotelController(HotelService hotelService) {
+    public HotelController(HotelService hotelService, HotelMapper hotelMapper) {
         this.hotelService = hotelService;
+        this.hotelMapper = hotelMapper;
     }
 
     @GetMapping
     public List<HotelDto> getHotels() {
         return hotelService.getHotels();
+    }
+
+    @PostMapping
+    public ResponseEntity<HotelDto> createHotel(@RequestBody CreateHotelRequest createHotelRequest) {
+        Hotel hotelToCreate = hotelMapper.toHotel(createHotelRequest);
+        Hotel createdHotel = hotelService.createHotel(hotelToCreate);
+
+        return ResponseEntity.ok(hotelMapper.toHotelDto(createdHotel));
     }
 }
