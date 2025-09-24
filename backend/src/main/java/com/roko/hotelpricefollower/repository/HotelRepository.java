b4c63af128a5c765fd2d538141df7126bc80f4f4
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-24 12:43:48 +0300

    Add query to find all hotels as HotelDto

--- backend/src/main/java/com/roko/hotelpricefollower/repository/HotelRepository.java
+++ backend/src/main/java/com/roko/hotelpricefollower/repository/HotelRepository.java
@@ -1,9 +1,23 @@
 package com.roko.hotelpricefollower.repository;
 
 import com.roko.hotelpricefollower.domain.Hotel;
+import com.roko.hotelpricefollower.dto.HotelDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
+
+    @Query("""
+        SELECT new com.roko.hotelpricefollower.dto.HotelDto(
+            h.id,
+            h.name,
+            h.imageUrl
+        )
+        FROM Hotel h
+    """)
+    List<HotelDto> findAllHotelDto();
 }
