e285cd901f6ab65c3e6ab32a29fac4e737ec09f7
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-25 19:57:23 +0300

    Add mapping to ScrapeProfile and a helper to parse first departure date from given scrape link

--- backend/src/main/java/com/roko/hotelpricefollower/mapper/ScrapeProfileMapper.java
+++ backend/src/main/java/com/roko/hotelpricefollower/mapper/ScrapeProfileMapper.java
@@ -1,8 +1,15 @@
 package com.roko.hotelpricefollower.mapper;
 
 import com.roko.hotelpricefollower.domain.ScrapeProfile;
+import com.roko.hotelpricefollower.dto.CreateScrapeProfileRequest;
 import com.roko.hotelpricefollower.dto.ScrapeProfileDto;
 import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ScrapeProfileMapper {
@@ -15,5 +22,29 @@ public class ScrapeProfileMapper {
                 scrapeProfile.getChildren(),
                 scrapeProfile.isActive()
         );
+    }
+
+    public ScrapeProfile toScrapeProfile(CreateScrapeProfileRequest createScrapeProfile) {
+        String scrapeUrl = createScrapeProfile.scrapeUrl();
+
+        return ScrapeProfile.builder()
+                .scrapeUrl(scrapeUrl)
+                .firstDepartureDate(parseFirstDepartureDate(scrapeUrl))
+                .durationWeeks(createScrapeProfile.durationWeeks())
+                .adults(createScrapeProfile.adults())
+                .children(createScrapeProfile.children())
+                .build();
+    }
+
+    private LocalDate parseFirstDepartureDate(String url) {
+        URI uri = URI.create(url);
+        List<String> query = List.of(uri.getQuery().split("&"));
+        String date = query.stream()
+                .map(s -> s.split("=", 2))
+                .collect(
+                        Collectors.toMap(a -> a[0], a -> a[1])
+                ).get("QueryDepDate");
+
+        return LocalDate.parse(date, DateTimeFormatter.BASIC_ISO_DATE);
     }
 }
