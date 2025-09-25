87c4625c8a4adde1d3c8203529cb14b66bb03001
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-25 17:09:11 +0300

    Add query to find all scrape profiles by hotel

--- backend/src/main/java/com/roko/hotelpricefollower/repository/ScrapeProfileRepository.java
+++ backend/src/main/java/com/roko/hotelpricefollower/repository/ScrapeProfileRepository.java
@@ -1,9 +1,13 @@
 package com.roko.hotelpricefollower.repository;
 
+import com.roko.hotelpricefollower.domain.Hotel;
 import com.roko.hotelpricefollower.domain.ScrapeProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ScrapeProfileRepository extends JpaRepository<ScrapeProfile,Long> {
+    List<ScrapeProfile> findAllByHotel(Hotel hotel);
 }
