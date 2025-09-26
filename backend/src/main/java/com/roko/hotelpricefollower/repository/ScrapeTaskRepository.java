5c4dfdedadf61c57fac75af677e865395b402ff5
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-26 15:59:06 +0300

    Add query to check if ScrapeTask with given parameters already exists

--- backend/src/main/java/com/roko/hotelpricefollower/repository/ScrapeTaskRepository.java
+++ backend/src/main/java/com/roko/hotelpricefollower/repository/ScrapeTaskRepository.java
@@ -1,9 +1,13 @@
 package com.roko.hotelpricefollower.repository;
 
+import com.roko.hotelpricefollower.domain.ScrapeProfile;
 import com.roko.hotelpricefollower.domain.ScrapeTask;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface ScrapeTaskRepository extends JpaRepository<ScrapeTask, Long> {
+    boolean existsByScrapeProfileAndFirstDepartureDate(ScrapeProfile scrapeProfile, LocalDate firstDepartureDate);
 }
