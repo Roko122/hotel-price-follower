d69644da8f96d35d419456fbd870ae706890ea8a
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-26 16:25:50 +0300

    Fix use correct annotations when validating integers

--- backend/src/main/java/com/roko/hotelpricefollower/dto/CreateScrapeProfileRequest.java
+++ backend/src/main/java/com/roko/hotelpricefollower/dto/CreateScrapeProfileRequest.java
@@ -1,19 +1,23 @@
 package com.roko.hotelpricefollower.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 public record CreateScrapeProfileRequest(
         @NotNull(message = "Duration cannot be null")
-        @Size(min = 1, max = 4, message = "Duration in weeks must be between {min} and {max}")
+        @Min(value = 1, message = "Duration in weeks must be at least {value}")
+        @Max(value = 4, message = "Duration in weeks must be at most {value}")
         Integer durationWeeks,
 
         @NotNull(message = "Amount of adults cannot be null")
-        @Size(min = 1, max = 8, message = "Amount of adults must be between {min} and {max}")
+        @Min(value = 1, message = "Amount of adults must be at least {value}")
+        @Max(value = 8, message = "Amount of adults must be at most {value}")
         Integer adults,
 
         @NotNull(message = "Amount of children cannot be null")
-        @Size(min = 0, max = 7, message = "Amount of children must be between {min} and {max}")
+        @Min(value = 0, message = "Amount of children must be at least {value}")
+        @Max(value = 7, message = "Amount of children must be at most {value}")
         Integer children
 ) {
 }
