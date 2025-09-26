9d674b70c53a0f5eedb7fff7c13b5f8c1ee3a62e
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-26 14:48:38 +0300

    Create ScrapeTaskMapper and move helper method from ScrapeProfileMapper to parse departure date from url

--- backend/src/main/java/com/roko/hotelpricefollower/mapper/ScrapeProfileMapper.java
+++ backend/src/main/java/com/roko/hotelpricefollower/mapper/ScrapeProfileMapper.java
@@ -5,12 +5,6 @@ import com.roko.hotelpricefollower.dto.CreateScrapeProfileRequest;
 import com.roko.hotelpricefollower.dto.ScrapeProfileDto;
 import org.springframework.stereotype.Component;
 
-import java.net.URI;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Component
 public class ScrapeProfileMapper {
 
@@ -29,17 +23,5 @@ public class ScrapeProfileMapper {
                 .adults(createScrapeProfile.adults())
                 .children(createScrapeProfile.children())
                 .build();
-    }
-
-    private LocalDate parseFirstDepartureDate(String url) {
-        URI uri = URI.create(url);
-        List<String> query = List.of(uri.getQuery().split("&"));
-        String date = query.stream()
-                .map(s -> s.split("=", 2))
-                .collect(
-                        Collectors.toMap(a -> a[0], a -> a[1])
-                ).get("QueryDepDate");
-
-        return LocalDate.parse(date, DateTimeFormatter.BASIC_ISO_DATE);
     }
 }
---
+++ backend/src/main/java/com/roko/hotelpricefollower/mapper/ScrapeTaskMapper.java
@@ -0,0 +1,44 @@
+package com.roko.hotelpricefollower.mapper;
+
+import com.roko.hotelpricefollower.domain.ScrapeTask;
+import com.roko.hotelpricefollower.dto.CreateScrapeTaskRequest;
+import com.roko.hotelpricefollower.dto.ScrapeTaskDto;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ScrapeTaskMapper {
+
+    public ScrapeTaskDto toScrapeTaskDto(ScrapeTask scrapeTask) {
+        String scrapeUrl = scrapeTask.getScrapeUrl();
+
+        return new ScrapeTaskDto(
+                scrapeTask.getId(),
+                scrapeUrl,
+                this.parseFirstDepartureDate(scrapeUrl)
+        );
+    }
+
+    public ScrapeTask toScrapeTask(CreateScrapeTaskRequest createScrapeTaskRequest) {
+        return ScrapeTask.builder()
+                .scrapeUrl(createScrapeTaskRequest.scrapeUrl())
+                .build();
+    }
+
+    private LocalDate parseFirstDepartureDate(String url) {
+        URI uri = URI.create(url);
+        List<String> query = List.of(uri.getQuery().split("&"));
+        String date = query.stream()
+                .map(s -> s.split("=", 2))
+                .collect(
+                        Collectors.toMap(a -> a[0], a -> a[1])
+                ).get("QueryDepDate");
+
+        return LocalDate.parse(date, DateTimeFormatter.BASIC_ISO_DATE);
+    }
+}
