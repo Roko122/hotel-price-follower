d1618bb0ac32ac1543b952d4c5951971eca01dee
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-26 13:59:36 +0300

    Add ScrapeProfileController with endpoints for getting all ScrapeProfiles and for adding new profiles

--- backend/src/main/java/com/roko/hotelpricefollower/controller/ScrapeProfileController.java
+++ backend/src/main/java/com/roko/hotelpricefollower/controller/ScrapeProfileController.java
@@ -1,37 +1,46 @@
 package com.roko.hotelpricefollower.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.roko.hotelpricefollower.domain.ScrapeProfile;
+import com.roko.hotelpricefollower.dto.CreateScrapeProfileRequest;
+import com.roko.hotelpricefollower.dto.ScrapeProfileDto;
+import com.roko.hotelpricefollower.mapper.ScrapeProfileMapper;
+import com.roko.hotelpricefollower.service.ScrapeProfileService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/hotels/{hotelId}/profiles")
 public class ScrapeProfileController {
-//    private final ScrapeProfileService scrapeProfileService;
-//    private final ScrapeProfileMapper scrapeProfileMapper;
-//
-//    public ScrapeProfileController(ScrapeProfileService scrapeProfileService, ScrapeProfileMapper scrapeProfileMapper) {
-//        this.scrapeProfileService = scrapeProfileService;
-//        this.scrapeProfileMapper = scrapeProfileMapper;
-//    }
-//
-//    @GetMapping
-//    public ResponseEntity<List<ScrapeProfileDto>> getProfiles(@PathVariable Long hotelId) {
-//        List<ScrapeProfile> scrapeProfiles = scrapeProfileService.getAllScrapeProfilesByHotel(hotelId);
-//        List<ScrapeProfileDto> scrapeProfileDtos = scrapeProfiles.stream()
-//                .map(scrapeProfileMapper::toScrapeProfileDto)
-//                .toList();
-//
-//        return ResponseEntity.ok(scrapeProfileDtos);
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<ScrapeProfileDto> createProfile(
-//            @PathVariable Long hotelId,
-//            @Valid @RequestBody CreateScrapeProfileRequest createScrapeProfileRequest) {
-//
-//        ScrapeProfile toCreate = scrapeProfileMapper.toScrapeProfile(createScrapeProfileRequest);
-//        ScrapeProfile createdScrapeProfile = scrapeProfileService.createScrapeProfile(hotelId, toCreate);
-//
-//        return null;
-//    }
+    private final ScrapeProfileService scrapeProfileService;
+    private final ScrapeProfileMapper scrapeProfileMapper;
+
+    public ScrapeProfileController(ScrapeProfileService scrapeProfileService, ScrapeProfileMapper scrapeProfileMapper) {
+        this.scrapeProfileService = scrapeProfileService;
+        this.scrapeProfileMapper = scrapeProfileMapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ScrapeProfileDto>> getProfiles(@PathVariable Long hotelId) {
+        List<ScrapeProfile> scrapeProfiles = scrapeProfileService.getAllScrapeProfilesByHotel(hotelId);
+        List<ScrapeProfileDto> scrapeProfileDtos = scrapeProfiles.stream()
+                .map(scrapeProfileMapper::toScrapeProfileDto)
+                .toList();
+
+        return ResponseEntity.ok(scrapeProfileDtos);
+    }
+
+    @PostMapping
+    public ResponseEntity<ScrapeProfileDto> createProfile(
+            @PathVariable Long hotelId,
+            @Valid @RequestBody CreateScrapeProfileRequest createScrapeProfileRequest) {
+
+        ScrapeProfile toCreate = scrapeProfileMapper.toScrapeProfile(createScrapeProfileRequest);
+        ScrapeProfile createdScrapeProfile = scrapeProfileService.createScrapeProfile(hotelId, toCreate);
+
+        return new ResponseEntity<>(scrapeProfileMapper.toScrapeProfileDto(createdScrapeProfile), HttpStatus.CREATED);
+    }
 }
