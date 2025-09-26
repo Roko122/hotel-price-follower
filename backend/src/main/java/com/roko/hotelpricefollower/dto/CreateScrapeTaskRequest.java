e81067ec680ba36a91a1939a7c41a574c33ce46d
rolfk <172577720+Roko122@users.noreply.github.com>
2025-09-26 16:21:16 +0300

    Fix url pattern check to contain "www"

--- backend/src/main/java/com/roko/hotelpricefollower/dto/CreateScrapeTaskRequest.java
+++ backend/src/main/java/com/roko/hotelpricefollower/dto/CreateScrapeTaskRequest.java
@@ -6,8 +6,8 @@ import org.hibernate.validator.constraints.URL;
 public record CreateScrapeTaskRequest(
         @URL(message = "Invalid URL format")
         @Pattern(
-                regexp = "^https://tjareborg\\.fi[a-zA-Z0-9\\-._~:/?#\\[\\]@!$&'()*+,;%=]+#hotel-search-app-root$",
-                message = "URL must start with https://tjareborg.fi, contain only valid URL characters " +
+                regexp = "^https://www\\.tjareborg\\.fi[a-zA-Z0-9\\-._~:/?#\\[\\]@!$&'()*+,;%=]+#hotel-search-app-root$",
+                message = "URL must start with https://www.tjareborg.fi, contain only valid URL characters " +
                         "and end in #hotel-search-app-root"
         )
         String scrapeUrl
