567bfc68ef97b2fdd844c61267162e5b09a46ef1
rolfk <172577720+Roko122@users.noreply.github.com>
2025-12-30 19:24:30 +0200

    Add hotel API client

---
+++ frontend/src/api/hotelApi.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import type { Hotel } from '../types/Hotel.ts';
+
+const baseUrl: string = 'http://localhost:8080/api/v1/hotels';
+
+async function getHotels(): Promise<Hotel[]> {
+  const res = await axios.get(baseUrl);
+  return res.data;
+}
+
+export default getHotels;
