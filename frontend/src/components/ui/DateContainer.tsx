1d336bd8b719b219c5979a089426bf5f7a382746
rolfk <172577720+Roko122@users.noreply.github.com>
2026-01-07 16:15:58 +0200

    Add DateContainer.tsx component to display departure dates

---
+++ frontend/src/components/ui/DateContainer.tsx
@@ -0,0 +1,21 @@
+import type { JSX } from 'react';
+import type { PriceSummary } from '@/types/Price.js';
+import { formatDateWithWeekday } from '@/util/dateConverter.js';
+
+type DateContainerProps = {
+  dates: PriceSummary[];
+};
+
+function DateContainer(props: DateContainerProps): JSX.Element {
+  const { dates } = props;
+
+  return (
+    <div className="flex justify-center text-center gap-12 font-bold mb-1">
+      {dates.map((date) => (
+        <span key={date.departureDate}>{formatDateWithWeekday(date.departureDate)}</span>
+      ))}
+    </div>
+  );
+}
+
+export default DateContainer;
