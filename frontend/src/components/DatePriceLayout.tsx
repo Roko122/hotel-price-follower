ee75b1ec95b13af88b1bc4536b89acb718a2ebdf
rolfk <172577720+Roko122@users.noreply.github.com>
2026-01-07 16:29:05 +0200

    Add DatePriceLayout component to display dates and prices

---
+++ frontend/src/components/DatePriceLayout.tsx
@@ -0,0 +1,25 @@
+import type { JSX } from 'react';
+import type { PriceSummary } from '@/types/Price.js';
+import DateContainer from '@/components/ui/DateContainer.js';
+import PriceTextContainer from '@/components/PriceTextContainer.js';
+
+type DatePriceLayoutProps = {
+  priceData: PriceSummary[];
+};
+
+function DatePriceLayout(props: DatePriceLayoutProps): JSX.Element {
+  const { priceData } = props;
+
+  return (
+    <>
+      <DateContainer dates={priceData} />
+      <div className="flex flex-col gap-5">
+        <PriceTextContainer prices={priceData} category="latestPrice" />
+        <PriceTextContainer prices={priceData} category="min30days" />
+        <PriceTextContainer prices={priceData} category="allTimeMin" />
+      </div>
+    </>
+  );
+}
+
+export default DatePriceLayout;
