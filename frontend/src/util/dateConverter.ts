afd4019b3fd995a7d81b87ff7f6e6c828a247625
rolfk <172577720+Roko122@users.noreply.github.com>
2026-01-07 16:14:06 +0200

    Add a util to add weekday in front of the date

--- frontend/src/util/dateConverter.ts
+++ frontend/src/util/dateConverter.ts
@@ -2,3 +2,11 @@ export const formatDate = (date: string): string => {
   const [year, month, day] = date.split('-');
   return `${day}.${month}.${year}`;
 };
+
+export const formatDateWithWeekday = (date: string): string => {
+  const weekdays: string[] = ['Su', 'Ma', 'Ti', 'Ke', 'To', 'Pe', 'La'];
+  const weekday: string = weekdays[new Date(date).getDay()];
+
+  const [_year, month, day] = date.split('-');
+  return `${weekday} ${day}.${month}.`;
+};
