b6ed47d74cd5218bf64a9541298b5f619c928bdc
rolfk <172577720+Roko122@users.noreply.github.com>
2026-01-07 16:28:25 +0200

    Add padding on the sides of date container

--- frontend/src/components/ui/DateContainer.tsx
+++ frontend/src/components/ui/DateContainer.tsx
@@ -10,7 +10,7 @@ function DateContainer(props: DateContainerProps): JSX.Element {
   const { dates } = props;
 
   return (
-    <div className="flex justify-center text-center gap-12 font-bold mb-1">
+    <div className="flex justify-center text-center gap-12 font-bold mb-1 px-10">
       {dates.map((date) => (
         <span key={date.departureDate}>{formatDateWithWeekday(date.departureDate)}</span>
       ))}
