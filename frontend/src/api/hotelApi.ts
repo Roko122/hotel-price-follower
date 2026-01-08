984460a288bd9e23c357a7eed9701cd7a0f6bf0a
rolfk <172577720+Roko122@users.noreply.github.com>
2026-01-08 16:29:59 +0200

    Add FetchTime type

--- frontend/src/api/hotelApi.ts
+++ frontend/src/api/hotelApi.ts
@@ -1,11 +1,16 @@
 import axios from 'axios';
 import type { Hotel } from '../types/Hotel';
+import type { FetchTime } from '@/types/FetchTime.js';
 
 const baseUrl: string = 'http://localhost:8080/api/v1/hotels';
 
-async function getHotels(): Promise<Hotel[]> {
+export async function getHotels(): Promise<Hotel[]> {
   const res = await axios.get(baseUrl);
   return res.data;
 }
 
-export default getHotels;
+export async function getFetchTime(hotelId: number): Promise<FetchTime> {
+  const url = `${baseUrl}/${hotelId}/last-fetch`;
+  const res = await axios.get(url);
+  return res.data;
+}
