c7838125f2ba72b7ef702cda641459e6fb765908
rolfk <172577720+Roko122@users.noreply.github.com>
2026-01-08 10:24:14 +0200

    Refactor price UI component to be more readable

--- frontend/src/components/ui/Price.tsx
+++ frontend/src/components/ui/Price.tsx
@@ -14,20 +14,37 @@ type PriceProps = {
 function Price(props: PriceProps): JSX.Element {
   const { price, date, soldOut, additionalInformation } = props;
 
-  const showTooltip: boolean = additionalInformation !== null;
-  const isAdditionalInformationPresent: boolean = additionalInformation === undefined;
+  const showTooltip: boolean = additionalInformation !== null || soldOut === true;
+
+  const priceToShow = (): string => {
+    if (soldOut || price === null) {
+      return '-';
+    } else {
+      return `${price} €`;
+    }
+  };
+
+  const tooltipContent = (): JSX.Element => {
+    if (soldOut) {
+      return <p>Loppuunmyyty</p>;
+    } else if (additionalInformation !== undefined && additionalInformation !== null) {
+      return <p>{additionalInformation.charAt(0).toUpperCase() + additionalInformation.slice(1)}</p>;
+    } else if (price === null) {
+      return <p>Ei hinta tietoja</p>;
+    } else {
+      return <p>Hinta {formatDate(date)}</p>;
+    }
+  };
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <p className="font-bold text-2xl text-center leading-none">{soldOut ? 'Loppuunmyyty' : `${price} €`}</p>
+      <p className="font-bold text-2xl text-center min-w-20">{priceToShow()}</p>
       {showTooltip && (
         <Tooltip>
           <TooltipTrigger asChild>
             <HelpCircle className="w-3.5 h-3.5 text-gray-500 hover:text-gray-600 cursor-help" />
           </TooltipTrigger>
-          <TooltipContent side="bottom">
-            {isAdditionalInformationPresent ? <p>Hinta {formatDate(date)}</p> : <p>{additionalInformation}</p>}
-          </TooltipContent>
+          <TooltipContent side="bottom">{tooltipContent()}</TooltipContent>
         </Tooltip>
       )}
     </div>
