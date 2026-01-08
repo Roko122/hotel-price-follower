308ac702b3ffaac975a4f135df02f2ec95ba27ec
rolfk <172577720+Roko122@users.noreply.github.com>
2026-01-08 13:19:06 +0200

    Move PriceTextContainer.tsx to components/ui

--- frontend/src/components/DatePriceLayout.tsx
+++ frontend/src/components/DatePriceLayout.tsx
@@ -1,7 +1,7 @@
 import type { JSX } from 'react';
 import type { PriceSummary } from '@/types/Price.js';
 import DateContainer from '@/components/ui/DateContainer.js';
-import PriceTextContainer from '@/components/PriceTextContainer.js';
+import PriceTextContainer from '@/components/ui/PriceTextContainer.js';
 
 type DatePriceLayoutProps = {
   priceData: PriceSummary[];
