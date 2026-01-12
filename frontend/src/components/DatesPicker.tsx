969a81d9757dfb0cfdfad3e814e4975e2f364e17
rolfk <172577720+Roko122@users.noreply.github.com>
2026-01-12 17:56:13 +0200

    Add a multi-select to pick 1-4 departure dates

---
+++ frontend/src/components/DatesPicker.tsx
@@ -0,0 +1,38 @@
+import {
+  MultiSelect,
+  MultiSelectContent,
+  MultiSelectGroup,
+  MultiSelectItem,
+  MultiSelectTrigger,
+  MultiSelectValue
+} from '@/components/ui/multi-select.js';
+
+import type { JSX } from 'react';
+import { formatDateWithWeekdayAndYear } from '@/util/dateConverter.js';
+
+type DatesPickerProps = {
+  dates: string[];
+};
+
+function DatesPicker(props: DatesPickerProps): JSX.Element {
+  const { dates } = props;
+
+  return (
+    <MultiSelect maxSelections={4}>
+      <MultiSelectTrigger className="w-full">
+        <MultiSelectValue placeholder="Valitse 1-4 lähtöpäivää" />
+      </MultiSelectTrigger>
+      <MultiSelectContent search={{ placeholder: 'Hae lähtöpäiviä', emptyMessage: 'Lähtöpäiviä ei löytynyt.' }}>
+        <MultiSelectGroup>
+          {dates?.map((date: string) => (
+            <MultiSelectItem key={date} value={date}>
+              {formatDateWithWeekdayAndYear(date)}
+            </MultiSelectItem>
+          ))}
+        </MultiSelectGroup>
+      </MultiSelectContent>
+    </MultiSelect>
+  );
+}
+
+export default DatesPicker;
